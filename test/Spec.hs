6d317ae209097ef1115c795d5cd00fba6351f528
Devin Lehmacher <lehmacdj@gmail.com>
2017-07-17 22:50:51 -0400

    implement a few simple tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,26 @@
+module Main where
+
+import Test.Tasty
+import Test.Tasty.HUnit
+
+import AST
+import Evaluator
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = defaultMain tests
+
+tests = testGroup "Evaluator" [unitTests]
+
+unitTests = testGroup "Unit Tests"
+    [ testCase "substitute var success" $
+        substitute "x" (Var "y") (Var "x") @?= Var "y"
+    , testCase "substitute var fail" $
+        substitute "x" (Var "y") (Var "z") @?= Var "z"
+    , testCase "substitute distributes over app" $
+        substitute "x" (Var "z")
+            (Var "x" `App` (Var "y" `App` Var "x"))
+        @?= (Var "z" `App` (Var "y" `App` Var "z"))
+    , testCase "substitute avoids λ's" $
+        substitute "x" (Var "y") (Lam "x" UnitTy (Var "x"))
+        @?= Lam "x" UnitTy (Var "x")
+    ]
