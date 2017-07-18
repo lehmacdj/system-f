3a86e7a678ce208cd65ee0b9afbf1a6191d727d1
Devin Lehmacher <lehmacdj@gmail.com>
2017-07-17 22:48:39 -0400

    AST: add instance for structural Eq

--- src/AST.hs
+++ src/AST.hs
@@ -1,12 +1,23 @@
+{-# LANGUAGE DeriveDataTypeable #-}
+
 module AST where
 
+import Data.Data
+import Control.Lens.Plated
+
+-- equality is purely structural
 data Term = Unit
           | Var String
           | Lam String Type Term
           | App Term Term
         -- | TyLam String Term
-        deriving (Show)
+        deriving (Show, Data, Typeable, Eq)
 
+instance Plated Term
+
+-- equality is purely structural
 data Type = UnitTy
           | FunTy Type Type
-          deriving (Show, Eq)
+        -- | Forall
+        -- | Exists
+          deriving (Show, Eq, Data, Typeable)
