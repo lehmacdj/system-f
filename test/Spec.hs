84ca7abff46b59ff818b83814152beadf0b9e806
Devin Lehmacher <lehmacdj@gmail.com>
2017-07-21 23:27:14 -0400

    implement more tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -12,18 +12,24 @@ import Variables
 main :: IO ()
 main = defaultMain tests
 
-tests = testGroup "Evaluator" [unitTests]
+tests = testGroup "Evaluator" [substituteTests]
 
-unitTests = testGroup "Unit Tests"
-    [ testCase "substitute Var success" $
+substituteTests = testGroup "substitute"
+    [ testCase "on Var succeeds" $
         substitute ("x" :: Var) (Var "y") (Var "x") @?= Var "y"
-    , testCase "substitute Var fail" $
+    , testCase "on Var fails" $
         substitute ("x" :: Var) (Var "y") (Var "z") @?= Var "z"
-    , testCase "substitute distributes over app" $
+    , testCase "distributes over app" $
         substitute ("x" :: Var) (Var "z")
             (Var "x" `App` (Var "y" `App` Var "x"))
         @?= (Var "z" `App` (Var "y" `App` Var "z"))
-    , testCase "substitute avoids λ's" $
-        substitute ("x" :: Var) (Var "y") (Lam (Variable "x") UnitTy (Var "x"))
-        @?= Lam (Variable "x") UnitTy (Var "x")
+    , testCase "avoids λ's" $
+        substitute ("x" :: Var) (Var "y") (Lam "x" UnitTy (Var "x"))
+        @?= Lam "x" UnitTy (Var "x")
+    , testCase "doesn't avoid nonbinding λ's" $
+        substitute ("z" :: Var) (Var "y") (Lam "x" UnitTy (Var "z"))
+        @?= Lam "x" UnitTy (Var "y")
+    , testCase "works on multiple variables" $
+        substitute ("x" :: Var) (Var "y") (Var "x" `App` Var "x")
+        @?= (Var "y" `App` Var "y")
     ]
